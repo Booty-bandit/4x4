bab3b41000d6e3172388b9d22ef2eda6a79b7019
alexherendaln <alexherendaln@gmail.com>
2025-12-08 11:17:58 +0100

    Update four_in_a_row_2d.py

--- four_in_a_row_2d.py
+++ four_in_a_row_2d.py
@@ -19,7 +19,40 @@ def mark(col, player):
     for row in board:
             row[col] = player
 
-            
+def check_row_wincon(map,row):
+    #counts the number of x's or y's in a row and resets the wincon_counter if there is a new type
+    current_wincon = ""
+    wincon_counter = 0
+    for y in range(collum):
+        for num in range(row):
+            if map[y][num] != current_wincon:
+                wincon_counter = 0
+            if map[y][num] == "x":
+                wincon_counter +=1
+                current_wincon = "x"
+            elif map[y][num] == "y":
+                wincon_counter +=1
+                current_wincon = "y"
+            if wincon_counter == 4:
+                print(f"{current_wincon} has won")
+
+def check_collum_wincon(map,collum):
+    current_wincon = ""
+    wincon_counter = 0
+    for x in range(row):
+        for num in range(collum):
+            if map[num][x] != current_wincon:
+                wincon_counter = 0
+            if map[num][x] == "x":
+                wincon_counter += 1
+                current_wincon = "x"
+            elif map[num][x] == "y":
+                wincon_counter += 1
+                current_wincon = "y"
+            if wincon_counter == 4:
+                print(f"{current_wincon} has won")
+
+
 def check_for_win(player):
     #Check rows
 
