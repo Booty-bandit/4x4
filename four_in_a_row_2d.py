1f5e8c7d20dc708ad7cdbac4d013a4c3edfa62cf
alexherendaln <alexherendaln@gmail.com>
2025-12-08 11:22:46 +0100

    Update four_in_a_row_2d.py

--- four_in_a_row_2d.py
+++ four_in_a_row_2d.py
@@ -2,10 +2,20 @@ empty = "."
 player1 = "0"
 player2 = "X"
 
-board =    [[empty, empty, empty, empty], 
+row = 5
+collum = 5
+board = []
+
+""" board =    [[empty, empty, empty, empty], 
             [empty, empty, empty, empty], 
             [empty, empty, empty, empty], 
-            [empty, empty, empty, empty]]
+            [empty, empty, empty, empty]] """
+
+def generate_board (row, collum):
+    for row in range(row):
+        board.append([])
+        for x in range(collum):
+            board[row].append(0)
 
 def print_board():
     print("| 1 | 2 | 3 | 4 |")
@@ -19,34 +29,34 @@ def mark(col, player):
     for row in board:
             row[col] = player
 
-def check_row_wincon(map,row):
+def check_row_wincon(board,row):
     #counts the number of x's or y's in a row and resets the wincon_counter if there is a new type
     current_wincon = ""
     wincon_counter = 0
     for y in range(collum):
         for num in range(row):
-            if map[y][num] != current_wincon:
+            if board[y][num] != current_wincon:
                 wincon_counter = 0
-            if map[y][num] == "x":
+            if board[y][num] == "x":
                 wincon_counter +=1
                 current_wincon = "x"
-            elif map[y][num] == "y":
+            elif board[y][num] == "y":
                 wincon_counter +=1
                 current_wincon = "y"
             if wincon_counter == 4:
                 print(f"{current_wincon} has won")
 
-def check_collum_wincon(map,collum):
+def check_collum_wincon(board,collum):
     current_wincon = ""
     wincon_counter = 0
     for x in range(row):
         for num in range(collum):
-            if map[num][x] != current_wincon:
+            if board[num][x] != current_wincon:
                 wincon_counter = 0
-            if map[num][x] == "x":
+            if board[num][x] == "x":
                 wincon_counter += 1
                 current_wincon = "x"
-            elif map[num][x] == "y":
+            elif board[num][x] == "y":
                 wincon_counter += 1
                 current_wincon = "y"
             if wincon_counter == 4:
@@ -55,8 +65,10 @@ def check_collum_wincon(map,collum):
 
 def check_for_win(player):
     #Check rows
+    check_row_wincon(board, row)
 
     #Check columns
+    check_collum_wincon(board, collum)
 
     #Check diagonal 1
 
